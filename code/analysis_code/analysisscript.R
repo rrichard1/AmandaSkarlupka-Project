d432ac433095d38d8b08c8b5771fa4c79ffe8802
Amanda <als80846@uga.edu>
2019-10-11 16:56:09 -0400

    Added text and analysis
    started analyzing the data and figuring out how to link the different folders. added text.

--- code/analysis_code/analysisscript.R
+++ code/analysis_code/analysisscript.R
@@ -8,24 +8,199 @@
 library(ggplot2)
 library(dplyr)
 library(broom)
+library(ggbiplot)
 
 #load data. path is relative to project directory.
-mydata <- readRDS("./data/processed_data/processeddata.rds")
+CA09_mabs <- readRDS("./data/processed_data/CA09mabs_processed_data.rds")
+CA09_sera <- readRDS("./data/processed_data/CA09sera_processed_data.rds")
+p1_mabs <- readRDS("./data/processed_data/p1mabs_processed_data.rds")
+p1_sera <- readRDS("./data/processed_data/p1sera_processed_data.rds")
+
+#I need these because I wrote the code before I realized that my data wasn't actually linked. So I'll need to go back and clean this up.
+CA09_mabs_full <- readRDS("./data/processed_data/CA09mabs_full_processed_data.rds")
+CA09_sera_full <- readRDS("./data/processed_data/CA09sera_full_processed_data.rds")
+p1_mabs_full <- readRDS("./data/processed_data/p1mabs_full_processed_data.rds")
+p1_sera_full <- readRDS("./data/processed_data/p1sera_full_processed_data.rds")
 
 #take a look at the data
-p1 <- mydata %>% ggplot(aes(x=Height, y=Weight)) + geom_point() + geom_smooth(method='lm')
+glimpse(CA09_mabs)
+glimpse(CA09_sera)
+glimpse(p1_mabs)
+glimpse(p1_sera)
 
-#save figure
-ggsave(filename="./results/resultfigure.png",plot=p1) 
 
+
+CA09_mabs <- CA09_mabs %>%
+  group_by(monoclonal) %>%
+  arrange(concentration)
   
-# fit linear model
-lmfit <- lm(Weight ~ Height, mydata)  
+  
+#Example graph to try and get the range of reaction of the monoclonal antibodies. 
+d <- CA09_mabs %>%
+  filter(monoclonal == "1E6") %>%
+  ggplot(aes(y = 1/concentration, x = antigen)) +
+  geom_point() +
+  scale_y_continuous(trans='log2') +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  labs(y = "Inverse Concentration")
 
-# place results from fit into a data frame with the tidy function
-lmtable <- broom::tidy(lmfit)
+#Graph of all the monoclonal responses
+CA09_monoclonal_responses <- CA09_mabs %>%
+  ggplot(aes(y = 1/concentration, x = antigen, color = lineage)) +
+  geom_point() +
+  scale_y_continuous(trans='log2') +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  labs(y = "Inverse Concentration") +
+  facet_wrap(vars(monoclonal), scales = "free_x")
 
-# save table  
-saveRDS(lmtable, file = "./results/resulttable.rds")
+ggsave(filename="./results/resultfigure1.png", plot = CA09_monoclonal_responses)
+
+p1_monoclonal_responses <- p1_mabs %>%
+  ggplot(aes(y = 1/concentration, x = antigen, color = lineage)) +
+  geom_point() +
+  scale_y_continuous(trans='log2') +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  labs(y = "Inverse Concentration") +
+  facet_wrap(vars(monoclonal), scales = "free_x")
+
+ggsave(filename="./results/resultfigure2.png", plot = p1_monoclonal_responses)
+
+p1_sera_responses <- p1_sera %>%
+  ggplot(aes(y = titer, x = antigen, color = lineage, alpha = 0.5)) +
+  geom_point() +
+  scale_y_continuous(trans='log2') +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  labs(y = "Inverse Concentration")
+
+ggsave(filename="./results/resultfigure3.png", plot = p1_sera_responses)
+
+CA09_sera_responses <- CA09_sera %>%
+  ggplot(aes(y = titer, x = antigen, color = lineage, alpha = 0.5)) +
+  geom_point() +
+  scale_y_continuous(trans='log2') +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  labs(y = "Inverse Concentration")
+
+ggsave(filename="./results/resultfigure4.png", plot = CA09_sera_responses)
+
+
+#graph <- function(x) {
+#CA09_mabs %>%
+ # group_by(monoclonal == x) %>%
+#  ggplot(aes(y = 1/concentration, x = reorder(antigen, geo_mean_CA09$geo_mean))) +
+  #geom_point() +
+#  scale_y_continuous(trans='log2') +
+#  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+ # labs(y = "Inverse Concentration")
+#}
+#This was my attempt to make a function that plotted everything. 
+CAmabs <- unique(CA09_mabs$monoclonal)
+
+for (i in CAmabs) {
+ d <- CA09_mabs %>%
+    filter(monoclonal == i) %>%
+    ggplot(aes(y = 1/concentration, x = reorder(antigen, geo_mean_CA09$geo_mean))) +
+    geom_point() +
+    scale_y_continuous(trans='log2', limits = (20)) +
+    theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+    labs(y = "Inverse Concentration", title = i)
+}
+
+
+#Principal component analysis on antibody responses to different viruses
+#Need to remove all the mice that do not have full matrix
+p1_sera_full <- p1_sera_full %>%
+  select(-c(4:7, 13:14, 16:17)) %>%
+  filter(mouse != 2 & mouse != 10)
+
+#Need to remove mice that do not have variability ofr a virus. 
+p1_sera_full_f <- p1_sera_full[,apply(p1_sera_full, 2, var, na.rm=TRUE) != 0]
+
+#now can do pca analysis
+p1_sera_full.pca <- prcomp(p1_sera_full_f, center = TRUE, scale = TRUE)
+summary(p1_sera_full.pca)
+str(p1_sera_full.pca)
+
+p1_sera_pca_variance <- plot(p1_sera_full.pca, type = "l")
+
+ggsave(filename="./results/resultfigure5.png", plot = p1_sera_pca_variance)
+
+g <- ggbiplot(p1_sera_full.pca, obs.scale = 1, var.scale = 1, groups = p1_sera_full_f$mouse)
+
+ggsave(filename="./results/resultfigure6.png", plot = g)
+
+#Repeat with CA09_sera
+#Need to remove all the mice that do not have full matrix
+CA09_sera_full_f <- CA09_sera_full %>%
+  select(-c(4:7, 13:14, 16:17))
+
+#Need to remove mice that do not have variability for a virus. 
+CA09_sera_full_f <- CA09_sera_full_f[,apply(CA09_sera_full_f, 2, var, na.rm=TRUE) != 0]
+
+#now can do pca analysis
+CA09_sera_full.pca <- prcomp(CA09_sera_full_f, center = TRUE, scale = TRUE)
+summary(CA09_sera_full.pca)
+str(CA09_sera_full.pca)
+
+CA09_sera_pca_variance <- plot(CA09_sera_full.pca, type = "l")
+
+ggsave(filename="./results/resultfigure7.png", plot = CA09_sera_pca_variance)
+
+h <- ggbiplot(CA09_sera_full.pca, obs.scale = 1, var.scale = 1, groups = CA09_sera_full_f$mouse)
+  
+ggsave(filename="./results/resultfigure8.png", plot = h)
+
+
+
+#There's not much variation, but if we add together the CA09_sera maybe we can see a little more variation
+CA09_sera_full <- CA09_sera_full %>%
+  select(-c(4:7, 13:14, 16:17))
+#renumber p1 mice so they don't overlap with the CA09 mice ids
+p1_sera_full$mouse <- c(7:14)
+
+sera_full <- bind_rows("CA09" = CA09_sera_full, "P1" = p1_sera_full, .id = "groups")
+#ok now that we have them all together and all the NAs are gone, we now need to get rid of the columns that have no variation.
+sera_full_f <- sera_full%>%
+  select(-c(1:2, 9:12))
+
+sera_full.pca <- prcomp(sera_full_f, center = TRUE, scale = TRUE)
+summary(sera_full.pca)
+str(p1_sera_full.pca)
+
+sera_pca_variance <- plot(sera_full.pca, type = "l")
+
+i <- ggbiplot(sera_full.pca, obs.scale = 1, var.scale = 1, groups = sera_full$groups, labels = sera_full$mouse)
+
+ggsave(filename="./results/resultfigure9.png", plot = i)
+
+#The plot above is also known as variable correlation plots. It shows the relationships between all variables. It can be interpreted as follow:
+
+#Positively correlated variables are grouped together.
+#egatively correlated variables are positioned on opposite sides of the plot origin (opposed quadrants).
+#The distance between variables and the origin measures the quality of the variables on the factor map. Variables that are away from the origin are well represented on the factor map.
+
+#I want to try this for the monoclonals now too.
+
+CA09_mabs_full <- CA09_mabs_full %>%
+  select(-17) %>%
+  na.omit()
+
+CA09_mabs_full <- as.numeric(as.factor(CA09_mabs_full$monoclonal))
+  
+
+#Need to remove mice that do not have variability ofr a virus. 
+CA09_mabs_full_f <- CA09_mabs_full[,apply(CA09_mabs_full, 2, var, na.rm=TRUE) != 0]
+
+#now can do pca analysis
+CA09_mabs_full.pca <- prcomp(CA09_mabs_full_f, center = TRUE, scale = TRUE)
+summary(CA09_mabs_full.pca)
+str(CA09_mabs_full.pca)
+
+CA09_mabs_pca_variance <- plot(CA09_mabs_full.pca, type = "l")
+
+ggsave(filename="./results/resultfigure5.png", plot = p1_sera_pca_variance)
+
+#renumber p1 mice so they don't overlap with the CA09 mice ids
+p1_sera_full$mouse <- c(7:14)
 
   
--- code/processing_code/processingscript.R
+++ code/processing_code/processingscript.R
@@ -1,17 +1,86 @@
 ###############################
 # processing script
-
+#load the packages needed
 library(readxl)
-P1_Mabs <- read_excel("data/raw_data/P1 Mabs.xlsx", 
+library(knitr)
+library(tidyverse)
+library(readxl)
+library(gtools)
+library(gdata)
+
+#load the data. The data is contained within one excel file, but is separated on different sheets
+p1_mabs_full <- read_excel("data/raw_data/P1 Mabs.xlsx", 
                       sheet = "P1 Mabs")
-CA09_Mabs <- read_excel("data/raw_data/P1 Mabs.xlsx", 
+CA09_mabs_full <- read_excel("data/raw_data/P1 Mabs.xlsx", 
                       sheet = "CA09 Mabs")
-P1_Sera <- read_excel("data/raw_data/P1 Mabs.xlsx", 
+p1_sera_full <- read_excel("data/raw_data/P1 Mabs.xlsx", 
                         sheet = "P1 Sera")
-CA09_Sera <- read_excel("data/raw_data/P1 Mabs.xlsx", 
+CA09_sera_full <- read_excel("data/raw_data/P1 Mabs.xlsx", 
                         sheet = "CA09 Sera")
 
-summary(P1_Mabs)
-summary(CA09_Mabs)
-summary(P1_Sera)
-summary(CA09_Sera)
+#Take a look at the data and make sure that it loaded correctly
+summary(p1_mabs_full)
+str(p1_mabs_full)
+summary(CA09_mabs_full)
+str(CA09_mabs_full)
+summary(p1_sera_full)
+str(p1_sera_full)
+summary(CA09_sera_full)
+str(CA09_sera_full)
+
+#Move the monoclonal antibody concentrations and the sera titers into the same column
+p1_mabs <- p1_mabs_full %>%
+  gather(key = "antigen", value = "concentration", 2:20)
+
+CA09_mabs <- CA09_mabs_full %>%
+  gather(key = "antigen", value = "concentration", 2:20)
+
+p1_sera <- p1_sera_full %>%
+  gather(key = "antigen", value = "titer", 2:20)
+
+CA09_sera <- CA09_sera_full %>%
+  gather(key = "antigen", value = "titer", 2:20)
+
+#The sera was not ran against all the viruses due to a limited supply. So the viruses that were used for sera testing are filtered below.
+CA09_sera <- CA09_sera %>%
+  na.omit()
+p1_sera <- p1_sera %>%
+  na.omit()
+
+#The concentrations are in two fold dilutions, the titers will be log2(value)
+p1_mabs$log2 <- log2(p1_mabs$concentration)
+
+CA09_mabs$log2 <- log2(CA09_mabs$concentration)
+
+p1_sera$log2 <- log2(p1_sera$titer)
+
+CA09_sera$log2 <- log2(CA09_sera$titer)
+
+#The lineages of the viruses need to be added
+
+antigen <- unique(CA09_mabs$antigen)
+lineage <- c("Eurasian", "Eurasian", "Classical", "Classical", "Classical", "Classical",
+             "Classical", "Classical", "Classical", "Classical", "Classical", "Human Seasonal",
+             "Human Seasonal", "Human Seasonal", "Human Seasonal", "Classical", "Classical", "Classical",
+             "Human Seasonal")
+
+lineage_key <- tibble(antigen, lineage)
+
+CA09_mabs <- left_join(CA09_mabs, lineage_key, by = "antigen")
+
+CA09_sera <- left_join(CA09_sera, lineage_key, by = "antigen")
+
+p1_mabs <- left_join(p1_mabs, lineage_key, by = "antigen")
+
+p1_sera <- left_join(p1_sera, lineage_key, by = "antigen")
+
+#save data as RDS
+saveRDS(CA09_mabs, file = "./data/processed_data/CA09mabs_processed_data.rds")
+saveRDS(CA09_sera, file = "./data/processed_data/CA09sera_processed_data.rds")
+saveRDS(p1_mabs, file = "./data/processed_data/p1mabs_processed_data.rds")
+saveRDS(p1_sera, file = "./data/processed_data/p1sera_processed_data.rds")
+saveRDS(p1_sera_full, file = "./data/processed_data/p1sera_full_processed_data.rds")
+saveRDS(CA09_sera_full, file = "./data/processed_data/CA09sera_full_processed_data.rds")
+saveRDS(p1_mabs_full, file = "./data/processed_data/p1mabs_full_processed_data.rds")
+saveRDS(CA09_mabs_full, file = "./data/processed_data/CA09mabs_full_processed_data.rds")
+
